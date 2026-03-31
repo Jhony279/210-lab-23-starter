ef04e0eeade7f116cd6dc517c5f071808cc58a9b
Jhony279 <lordjhony732@gmail.comm>
2026-03-31 15:09:48 -0700

    Milestone 1 complete, 3 aditional constructors added

--- Goat.h
+++ Goat.h
@@ -12,6 +12,10 @@ private:
     string color;
 public: 
     Goat()                          { name = ""; age = 0; color = ""; }
+    Goat(string n)                  {name = n; age = 0; color = "";}
+    Goat(string n, int a)           {name = n; age = a; color = "";}
+    Goat(string n, int a, string c) {name = n; age = a; color = c;}
+
     // write three more constructors
 
     // setters and getters
